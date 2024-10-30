bd403a2fab2c32599a817ba19ad299928232bc76
Zulfaabam <zulfafatahakbar@gmail.com>
2024-10-30 20:37:24 +0700

    wip: page transition animation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  withViewTransitions,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withComponentInputBinding()),
+    provideRouter(routes, withComponentInputBinding(), withViewTransitions()),
     provideClientHydration(),
     provideHttpClient(withFetch()),
+    provideAnimations(),
   ],
 };
